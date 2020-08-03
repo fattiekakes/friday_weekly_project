76686e2cff29ddfae6f4a6e49d9b07f38320759f
fattiekakes <phat.ng92@gmail.com>
2020-08-03 10:49:03 +0700

    Week1 assignmen

--- Week1WebScraping.py
+++ Week1WebScraping.py
@@ -20,36 +20,51 @@ def scrape_tiki(url="https://tiki.vn/dien-thoai-may-tinh-bang/c1789?_lc=Vk4wMzkw
 
     # Extract information of each article
     for product in products:
-
-        d = {'seller_id':'','product_id':'','title':'','price':'','image_url':''}
+        
+        d = {
+            'seller_id':'',
+            'product_brand':'',
+            'product_id':'',
+            'product_title':'',
+            'price_included_sale':'',
+            'image_url':'',
+            'sale_percentage':''
+        }
 
         try:
             d['seller_id'] = product['data-seller-product-id']
+            d['product_brand'] = product['data-brand']
             d['product_id'] = product['product-sku']
-            d['title'] = product['data-title']
-            d['price'] = product['data-price'] 
+            d['product_title'] = product['data-title']
+            d['price_included_sale'] = product['data-price'] 
             d['image_url'] = product.find('span', {'class':'image'}).img['src']
 
+            #check if the price include sale or not
+            sale_tag = product.find('span', {'class':'sale-tag sale-tag-square'}).text
+            
+            if sale_tag:
+                d['sale_percentage'] = sale_tag
 
             #add to product_data array:
             data.append(d)
             
         except:
             # Skip if error and print error message
-            print("We got one product error!")
+            pass
 
     return data
 
+# Scolling to different pages
 result = []
+from time import sleep
+duration = 2
+
 for i in range(10):
     data = scrape_tiki('https://tiki.vn/dien-thoai-may-tinh-bang/c1789?_lc=&page='+str(i))
     result += data
+    sleep(duration)
     
-from time import sleep
-duration = 2
-sleep(duration)
-    
+# Print out
 import pandas as pd
 
 print(pd.DataFrame(data = result, columns = result[0].keys()))
-
